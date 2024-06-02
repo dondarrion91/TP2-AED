baf0b7fa968e04388fcadf0c2a5a18d2c559f716
julian.sanchez1 <julian.sanchez@silstech.com.ar>
2024-06-02 14:23:46 -0300

    Agrego logica de calculo de monto inicial y final hecha en el tp1 pero aplico entrada de datos de text por archivos

--- main.py
+++ main.py
@@ -1,29 +1,5 @@
 __author__ = "TP2-G023"
 
-envios = open("envios.txt", "rt")
-primera_linea = True
-
-for envio_cad in envios:
-    if not primera_linea:
-        cp = ""
-        direccion = envio_cad[9:29]
-        tipo = envio_cad[29]
-        pago = envio_cad[30]
-
-        for car in envio_cad[0:9]:
-            if car != " ":
-                cp += car
-
-        print("\nCodigo postal", cp)
-        print("Direccion:", direccion)
-        print("Tipo de envio:", tipo)
-        print("Forma de pago:", pago)
-
-    primera_linea = False
-
-"""
-inicial = 0
-
 ISO_3166_2_AR_DIGITS = "ABCDEFGHJKLMNPQRSTUVWXYZ"
 
 ISO_3166_2_AR_DIGITS_NAMES = (
@@ -63,76 +39,139 @@ MONTOS_ENVIOS = (
     (6, 17900),
 )
 
-# Punto 1)
-isArgentina = ((len(cp) == 8 and
-               (cp[0].upper() in ISO_3166_2_AR_DIGITS) and
-               len(cp[1:5]) == 4 and
-               cp[1:5].isdigit() and
-               len(cp[5:]) == 3) and
-               cp[5:].isalpha())
+def cp_is_argentina(cp):
+    return ((len(cp) == 8 and
+                   (cp[0].upper() in ISO_3166_2_AR_DIGITS) and
+                   len(cp[1:5]) == 4 and
+                   cp[1:5].isdigit() and
+                   len(cp[5:]) == 3) and
+                   cp[5:].isalpha())
+    
+def cp_is_bolivia(cp):
+    return len(cp) == 4 and cp.isdigit()
 
-isBolivia = len(cp) == 4 and cp.isdigit()
+def cp_is_brasil(cp):
+    return len(cp) == 9 and cp[:5].isdigit() and cp[5] == "-" and cp[6:].isdigit()
 
-isBrasil = len(cp) == 9 and cp[:5].isdigit() and cp[5] == "-" and cp[6:].isdigit()
+def cp_is_chile(cp):
+    return len(cp) == 7 and cp.isdigit()
 
-isChile = len(cp) == 7 and cp.isdigit()
+def cp_is_paraguay(cp):
+    return len(cp) == 6 and cp.isdigit()
 
-isParaguay = len(cp) == 6 and cp.isdigit()
+def cp_is_uruguay(cp):
+    return len(cp) == 5 and cp.isdigit()
 
-isUruguay = len(cp) == 5 and cp.isdigit()
+# Seteo el país de destino según el formato del código postal.
+def setear_destino(cp):
+    isArgentina = cp_is_argentina(cp)
+    isBolivia = cp_is_bolivia(cp)
+    isBrasil = cp_is_brasil(cp)
+    isChile = cp_is_chile(cp)
+    isParaguay = cp_is_paraguay(cp)
+    isUruguay = cp_is_uruguay(cp)
 
+    if isArgentina:
+        return "Argentina"
 
-if isArgentina:
-    destino = "Argentina"
-elif isBolivia:
-    destino = "Bolivia"
-elif isBrasil:
-    destino = "Brasil"
-elif isChile:
-    destino = "Chile"
-elif isParaguay:
-    destino = "Paraguay"
-elif isUruguay:
-    destino = "Uruguay"
-else:
-    destino = "Otro"
+    if isBolivia:
+        return "Bolivia"
 
-# Punto 2)
-primer_digito_o_letra = cp[0]
+    if isBrasil:
+        return "Brasil"
 
-provincia = "No aplica"
+    if isChile:
+        return "Chile"
 
-if isArgentina:
-    for iso_digit_name in ISO_3166_2_AR_DIGITS_NAMES:
-        if primer_digito_o_letra == iso_digit_name[0]:
-            provincia = iso_digit_name[1]
+    if isParaguay:
+        return "Paraguay"
 
-# Punto 3)
-for monto in MONTOS_ENVIOS:
-    if tipo == monto[0]:
-        inicial = monto[1]
+    if isUruguay:
+        return "Uruguay"
 
-if not isArgentina:
+    return "Otro"
+
+# Elijo la provincia(en caso de ser envio en argentina) según el código postal.
+def set_provincia(cp):
+    primer_digito_o_letra = cp[0]
+
+    is_argentina = cp_is_argentina(cp)
+    provincia = "No aplica"
+
+    if is_argentina:
+        for iso_digit_name in ISO_3166_2_AR_DIGITS_NAMES:
+            if primer_digito_o_letra == iso_digit_name[0]:
+                provincia = iso_digit_name[1]
+
+    return provincia
+
+# Calculo el monto inicial a pagar según el tipo de envio y cp
+def set_monto_inicial(cp, tipo):
+    primer_digito_o_letra = cp[0]
+    inicial = 0
+
+    for monto in MONTOS_ENVIOS:
+        if tipo == monto[0]:
+            inicial = monto[1]
+
+    isArgentina = cp_is_argentina(cp)
+    isBolivia = cp_is_bolivia(cp)
+    isBrasil = cp_is_brasil(cp)
+    isChile = cp_is_chile(cp)
+    isParaguay = cp_is_paraguay(cp)
+    isUruguay = cp_is_paraguay(cp)
+
+    if isArgentina:
+        return inicial
+
     if (isBolivia or isParaguay or (isUruguay and int(primer_digito_o_letra) == 1) or
             (isBrasil and int(primer_digito_o_letra) in (8, 9))):
-        inicial = int(inicial * 1.20)
-    elif (isChile or (isUruguay and int(primer_digito_o_letra) != 1) or
+        return int(inicial * 1.20)
+
+    if (isChile or (isUruguay and int(primer_digito_o_letra) != 1) or
           (isBrasil and int(primer_digito_o_letra) in (0, 1, 2, 3))):
-        inicial = int(inicial * 1.25)
-    elif isBrasil and int(primer_digito_o_letra) in (4, 5, 6, 7):
-        inicial = int(inicial * 1.30)
-    else:
-        inicial = int(inicial * 1.50)
+        return int(inicial * 1.25)
 
-# Punto 4)
-final = inicial
+    if isBrasil and int(primer_digito_o_letra) in (4, 5, 6, 7):
+        return int(inicial * 1.30)
 
-if pago == 1:
-    final = int(inicial * 0.90)
+    return int(inicial * 1.50)
 
+# Calculo monto final según el metodo de pago del envio.
+def set_monto_final(inicial, pago):
+    if pago == 1:
+        return int(inicial * 0.90)
+    
+    return inicial
 
-print("País de destino del envío:", destino)
-print("Provincia destino:", provincia)
-print("Importe inicial a pagar:", inicial)
-print("Importe final a pagar:", final)
-"""
+envios = open("envios.txt", "rt")
+primera_linea = True
+
+# Cuerpo principal del script
+for envio_cad in envios:
+    if not primera_linea:
+        cp = ""
+        direccion = envio_cad[9:29]
+        tipo = int(envio_cad[29])
+        pago = int(envio_cad[30])
+
+        # Elimino los espacios del código postal.
+        for car in envio_cad[0:9]:
+            if car != " ":
+                cp += car
+
+        destino = setear_destino(cp)
+        provincia = set_provincia(cp)
+        inicial = set_monto_inicial(cp, tipo)
+        final = set_monto_final(inicial, pago)
+
+        print("\nCodigo postal", cp)
+        print("Direccion:", direccion)
+        print("Tipo de envio:", tipo)
+        print("Forma de pago:", pago)
+        print("Destino:", destino)
+        print("Provincia:", provincia)
+        print("Monto incial:", inicial)
+        print("Monto final:", final)
+
+    primera_linea = False
